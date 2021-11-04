9f45e63234caaf6c9bbe83a6ad6445234450fb53
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-11-04 18:17:13 +0900

    FEAT: Add URLMap model

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class URLMap(TimeStampedModel):
+    """
+    Create a short url from original_url
+    Save original_url with short_url
+    """
+
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return f'original_url: {self.original_url}, short_url: {self.short_url}'
+
