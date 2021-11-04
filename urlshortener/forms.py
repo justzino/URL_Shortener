ff433fe53497c592b0ad7cfac2e8314e1ba9068e
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-11-04 19:02:19 +0900

    FEAT: 입력 받는 form 변경

--- urlshortener/forms.py
+++ urlshortener/forms.py
@@ -4,9 +4,10 @@ from .models import URLMap
 
 
 class ShortenerForm(forms.ModelForm):
-    original_url = forms.URLField()
+    original_url = forms.URLField(widget=forms.URLInput(
+        attrs={"class": "form-control form-control-lg", "placeholder": "URL을 입력하세요."}
+    ))
 
     class Meta:
         model = URLMap
         fields = ('original_url',)
-
